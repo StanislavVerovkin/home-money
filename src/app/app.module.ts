4970379c7b9a2cc0f7d9d7d7f87115f9bf1c6925
sverevkin25@gmail.com <82106610Vereva>
2018-11-01 18:32:04 +0200

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
-import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
 import {UserService} from './shared/services/user.service';
+import {AuthService} from './shared/services/auth.service';
+import {HttpClientModule} from '@angular/common/http';
+import {SystemModule} from './system/system.module';
+import {SystemRoutingModule} from './system/system-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    AuthModule,
-    HttpModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [UserService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        AuthModule,
+        HttpClientModule,
+        AppRoutingModule,
+        SystemModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [UserService, AuthService],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,20 +1,23 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs';
 import {User} from '../models/user.model';
 import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: Http) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getUserByEmail(email: string): Observable<User> {
-    return this.http.get(`http://localhost:3000/users?email=${email}`)
-      .pipe(map((response: Response) => response.json()))
-      .pipe(map((user: User[]) => user[0] ? user[0] : undefined));
-  }
+    getUserByEmail(email: string): Observable<User> {
+        return this.http.get(`http://localhost:3000/users?email=${email}`)
+            .pipe(map((user: User[]) => user[0] ? user[0] : undefined));
+    }
+
+    createNewUser(user: User): Observable<any> {
+       return this.http.post('http://localhost:3000/users', user);
+    }
 }
