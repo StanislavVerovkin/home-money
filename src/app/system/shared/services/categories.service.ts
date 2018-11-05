4d68d8f83971f5ffaf7e7d60b90a3c0faf131d2a
Stanislav Verovkin <sverevkin25@gmail.com>
2018-11-05 09:18:56 +0200

    second

---
+++ src/app/system/shared/services/categories.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {BaseApi} from '../../../shared/core/base-api';
+import {HttpClient} from '@angular/common/http';
+import {Category} from '../models/category.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CategoriesService extends BaseApi {
+
+    constructor(public http: HttpClient) {
+        super(http);
+    }
+
+    addCategory(category: Category): Observable<Category> {
+        return this.post('categories', category);
+    }
+}
